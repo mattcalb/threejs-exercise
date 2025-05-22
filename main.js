8a30182ae9f825b761cc6562b271f471328616cc
mattcalb <mattcalb1@gmail.com>
2025-05-22 15:43:13 -0300

    change camera initial position

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
-camera.position.set(5, 5, 10);
+camera.position.set(20, 20, 10);
 camera.lookAt(0, 0, 0);
 
 // renderer
